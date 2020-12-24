b148c3e6e7d883ac53d160480644ef231a737f33
toxicmuffin <toxicmuffin101@hotmail.com>
2020-12-24 11:11:52 -0600

    test: moved form to its own component

--- pages/Contact/index.vue
+++ pages/Contact/index.vue
@@ -10,37 +10,7 @@
         photography related, I’m doing something with code or design.
       </p>
     </div>
-    <form
-      name="contact"
-      action="/success/"
-      method="POST"
-      enctype="Content-Type of application/x-www-form-urlencoded"
-      netlify-honeypot="bot-field"
-      data-netlify-recaptcha="true"
-      data-netlify="true"
-    >
-      <input type="hidden" name="contact-form" value="contact" />
-      <label for="person-name">Name</label>
-      <input id="name-field" type="text" name="person-name" required />
-      <label for="email">Email</label>
-      <input id="email-field" type="email" name="email" required />
-      <label for="message">Message</label>
-      <textarea
-        id="message-field"
-        name="message"
-        cols="30"
-        rows="10"
-        required
-      ></textarea>
-      <p class="hidden">
-        <label class="hidden">
-          Don’t fill this out if you’re human:
-          <input name="bot-field" />
-        </label>
-      </p>
-      <div data-netlify-recaptcha="true"></div>
-      <button id="send-it" type="submit">Send</button>
-    </form>
+    <ContactForm />
   </div>
 </template>
 
